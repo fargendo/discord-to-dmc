dc880367183dd54d03bbaf75a07f78be7b3db60e
fargendo <landrum287@hotmail.com>
2020-06-06 23:31:30 +0000

    fix spacing

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ client.on('message', message => {
 		if (message.author.id !== '706667912613593098') {
 			console.log('message received from #server_chat')
 			const msg = {
-				message: '[' + message.author.username + ']:  ' + message.content,
+				message: '[' + message.author.username + ']: ' + message.content,
 				type: 'dmcchat',
 			}
 			ws.send(JSON.stringify(msg))
@@ -30,7 +30,7 @@ client.on('message', message => {
 		if (message.author.id !== '718914956023824518') {
 			console.log('message received from donfuer #party_chat')
 			const msg = {
-				message: '[' + message.author.username + '] ' + message.content,
+				message: '[' + message.author.username + ']: ' + message.content,
 				type: 'donfuer',
 			}
 			ws.send(JSON.stringify(msg))
