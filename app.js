6092587813c55132ae2c2d3adea6147d662b97dd
fargendo <landrum287@hotmail.com>
2020-09-28 21:13:51 -0500

    remove whygood troll

--- app.js
+++ app.js
@@ -39,9 +39,6 @@ client.on('message', message => {
 				ws.send(JSON.stringify(msg))
 			}
 		}
-		if (message.author.id == '163016436854423553') {
-			message.author.send('Fuck you whygood')
-		}
 	}
 })
 
