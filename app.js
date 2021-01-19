389abbea0658731fcd4a4538f334051698c3d7c5
fargendo <landrum287@hotmail.com>
2021-01-19 10:07:55 -0600

    add chat and presence support

--- app.js
+++ app.js
@@ -7,33 +7,50 @@ const client = new Discord.Client()
 const URL = 'ws://localhost:9000'
 ws = new WebSocket(URL)
 
+const server_chat_channel_id = '800076482357231657'
+const webhook_id = '717064852363018300'
+let numberOfPlayers
+
+ws.on('message', function incoming(data) {
+	const payload = JSON.parse(data)
+	const channel = client.channels.cache.get(server_chat_channel_id)
+	let combinedMessage
+
+	if (payload.type === 'chat') {
+		const username = payload.username
+		const msg = payload.message
+		if (
+			(username.startsWith('_') && username.endsWith('_')) ||
+			(username.startsWith('__') && username.endsWith('__'))
+		) {
+			combinedMessage = `<**\\${username}**> ${msg}`
+		} else {
+			combinedMessage = `<**${username}**> ${msg}`
+		}
+
+		channel.send(combinedMessage)
+	}
+	if (payload.type === 'playersLength') {
+		console.log(payload.playersLength)
+		numberOfPlayers = payload.playersLength
+		client.user.setActivity(`NA with ${numberOfPlayers} online`)
+	}
+})
+
 client.once('ready', () => {
 	console.log('Discord bot online!')
 })
 
 client.on('message', message => {
 	// require messages to be in #server_chat
-	if (message.channel.id === '703037445201068032') {
+	if (message.channel.id === server_chat_channel_id) {
 		// parse messages from webhook bot
-		if (message.author.id !== '706667912613593098') {
+
+		if (message.author.id !== webhook_id) {
 			console.log('message received from #server_chat')
 			const msg = {
 				message: '[' + message.author.username + ']: ' + message.content,
-				type: 'dmcchat',
-			}
-			if (msg.message.length <= 256) {
-				ws.send(JSON.stringify(msg))
-			}
-		}
-	}
-	// Don fuer Party chat
-	if (message.channel.id === '718912872432009216') {
-		// webhook bot
-		if (message.author.id !== '718914956023824518' && message.author.id !== '163016436854423553') {
-			console.log('message received from donfuer #party_chat')
-			const msg = {
-				message: '[' + message.author.username + ']: ' + message.content,
-				type: 'donfuer',
+				type: 'na_chat',
 			}
 			if (msg.message.length <= 256) {
 				ws.send(JSON.stringify(msg))
